1701d605662e4034a3ef59f20598b2991da727f0
stovermc <stovermc@gmail.com>
2017-07-17 11:34:40 -0600

    Add flash messages to application.html
    * add test to successfully create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Success! New Account Created."
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -21,7 +21,17 @@ describe "user authentication" do
         expect(page).to have_content 'Password'
         expect(page).to have_content 'Password confirmation'
       end
+    end
 
+    it "redirects to link index upon account creation" do
+      visit new_user_path
+
+      fill_in 'Email', with: 'stovermc@gmail.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with:'password'
+      click_on 'Create Account'
+
+      expect(current_path).to eq root_path
     end
   end
 end
